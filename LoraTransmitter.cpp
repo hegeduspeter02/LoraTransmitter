65d4992f78d30d6131f790cde51998f81ec83154
Hegedűs Péter <hegipeti25@gmail.com>
2025-03-10 11:13:25 +0100

    optimized code for the receiver

--- LoraTransmitter.cpp
+++ LoraTransmitter.cpp
@@ -75,9 +75,9 @@ CayenneLPP convertMeasureDataToLowPowerPayload(const MeasureData &measureData)
   CayenneLPP lpp(PAYLOAD_SIZE);
   lpp.reset();
 
-  lpp.addTemperature(BME_280_SENSOR_IDENTIFIER, measureData.temperature);
-  lpp.addRelativeHumidity(BME_280_SENSOR_IDENTIFIER, measureData.humidity);
-  lpp.addBarometricPressure(BME_280_SENSOR_IDENTIFIER, measureData.pressure);
+  lpp.addTemperature(BME_280_TEMPERATURE_SENSOR_IDENTIFIER, measureData.temperature);
+  lpp.addRelativeHumidity(BME_280_HUMIDITY_SENSOR_IDENTIFIER, measureData.humidity);
+  lpp.addBarometricPressure(BME_280_PRESSURE_SENSOR_IDENTIFIER, measureData.pressure);
   lpp.addDigitalInput(UV_SENSOR_IDENTIFIER, measureData.uvIndex);
   lpp.addPercentage(SOIL_MOISTURE_SENSOR_IDENTIFIER, measureData.soilMoisture);
   lpp.addPercentage(RAIN_SENSOR_IDENTIFIER, measureData.rainPercent);
